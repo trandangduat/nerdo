43b4baeb16e8c7c5a0a1d6a2499acff556d8d306
Tran Dang Duat <23020023@vnu.edu.vn>
2025-01-08 22:10:42 +0700

    Update reminder time format from DD-MM-YY to DD/MM/YY

--- bot_msg.js
+++ bot_msg.js
@@ -1,9 +1,9 @@
 const NOTE = "Xem danh sách lời nhắc và thêm, xoá, sửa bằng lệnh '/start'.";
 
-export const ADD_REMINDER_INSTRUCTION = "Vui lòng gửi lời nhắc theo định dạng:\n\n`<DD-MM-YY> <hh:mm> <Nội dung lời nhắc>`\n\nVí dụ: `25-12-23 18:30 Mua quà Giáng Sinh`";
+export const ADD_REMINDER_INSTRUCTION = "Vui lòng gửi lời nhắc theo định dạng:\n\n`<DD/MM/YY> <hh:mm> <Nội dung lời nhắc>`\n\nVí dụ: `01/02/25 18:30 Đi mua đồ ăn`";
 export const REMOVE_REMINDER_INSTRUCTION = "Vui lòng gửi ID của lời nhắc bạn muốn xoá.";
 export const EDIT_REMINDER_ID_ASK = "Vui lòng gửi ID của lời nhắc bạn muốn sửa.";
-export const EDIT_REMINDER_INSTRUCTION = "Vui lòng gửi lời nhắc mới theo định dạng:\n\n`<DD-MM-YY> <hh:mm> <Nội dung lời nhắc>`";
+export const EDIT_REMINDER_INSTRUCTION = "Vui lòng gửi lời nhắc mới theo định dạng:\n\n`<DD/MM/YY> <hh:mm> <Nội dung lời nhắc>`";
 export const REMINDER_SAVED_SUCCESS = `Đã lưu thành công lời nhắc!\n${NOTE}`;
 export const REMINDER_DELETED_SUCCESS = `Đã xoá thành công lời nhắc!\n${NOTE}`;
 export const REMINDER_EDITED_SUCCESS = `Đã sửa thành công lời nhắc!\n${NOTE}`;
--- utils.js
+++ utils.js
@@ -1,24 +1,24 @@
 /**
- * Trích lời nhắc từ định dạng <DD-MM-YY> <hh:mm> <Nội dung lời nhắc>
+ * Trích lời nhắc từ định dạng <DD/MM/YY> <hh:mm> <Nội dung lời nhắc>
  */
 export const parseReminder = (text) => {
-    const regex = /(\d{2}-\d{2}-\d{2})\s+(\d{2}:\d{2})\s+(.+)/;
+    const regex = /(\d{2}\/\d{2}\/\d{2})\s+(\d{2}:\d{2})\s+(.+)/;
     const match = text.match(regex);
     if (!match) {
-        throw new Error("Invalid reminder format. Expected format: <DD-MM-YY> <hh:mm> <Nội dung lời nhắc>");
+        throw new Error("Invalid reminder format. Expected format: <DD/MM/YY> <hh:mm> <Nội dung lời nhắc>");
     }
     const [_, date, time, content] = match;
-    const [day, month, year] = date.split('-').map(Number);
+    const [day, month, year] = date.split('/').map(Number);
     const [hours, minutes] = time.split(':').map(Number);
     const notiTime = new Date(year + 2000, month - 1, day, hours, minutes);
     return { content, notiTime };
 }
-export const toReminderString = (reminder, notiTime) => { // Convert to format: DD-MM-YY hh:mm reminder
+export const toReminderString = (reminder, notiTime) => { // Convert to format: DD/MM/YY hh:mm reminder
     const date = notiTime.toLocaleString("en-GB", {
         day: "2-digit",
         month: "2-digit",
         year: "2-digit"
-    }).replace(/\//g, '-');
+    });
     const time = notiTime.toLocaleString("en-GB", {
         hour: "2-digit",
         minute: "2-digit"
