54b80847e466724121c0fee0fbac51b85ddfcac1
Tran Dang Duat <23020023@vnu.edu.vn>
2025-01-14 15:03:25 +0700

    Add character set and collation to database for Vietnamese support

--- db.sql
+++ db.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS nerdo;
+CREATE DATABASE IF NOT EXISTS nerdo CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE nerdo;
 
 CREATE TABLE Users (
@@ -6,7 +6,7 @@ CREATE TABLE Users (
     userId BIGINT,
     utcOffset INT,
     PRIMARY KEY (chatId, userId)
-);
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE Reminders (
     id BIGINT AUTO_INCREMENT PRIMARY KEY, -- Unique identifier for each reminder
@@ -16,4 +16,4 @@ CREATE TABLE Reminders (
     timeAdded TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Timestamp when the reminder is added
     notiTime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, -- Notification time for the reminder
     isNotified BOOLEAN DEFAULT FALSE      -- Whether the reminder has been notified (optional)
-);
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
